fd7afc08132dfe584ddb36afd5bb65349f286847
Melvin Viougea <mviougea@icloud.com>
2025-03-07 10:09:36 +0100

    language update

--- components/render/HomeRender.tsx
+++ components/render/HomeRender.tsx
@@ -1,8 +1,10 @@
-import React, {FC} from "react";
-import {formatToPrice} from "@/lib/function";
+import React, { FC } from "react";
+import { formatToPrice } from "@/lib/function";
+import { useLanguage } from "@/context/LanguageProvider";
 
-const HomeRender: FC<RenderProps> = ({profileData}) => {
+const HomeRender: FC<RenderProps> = ({ profileData }) => {
   if (!profileData) return null;
+  const { language } = useLanguage();
 
   return (
     <div>
@@ -10,31 +12,69 @@ const HomeRender: FC<RenderProps> = ({profileData}) => {
         <h1 className="text-3xl font-extrabold underline decoration-wavy decoration-yellow decoration-4 underline-offset-8 mb-8">
           Skills :
         </h1>
-        <p className="font-bold">Farming {profileData.farmingLvl} / {profileData.farmingMaxLvl}</p>
-        <p className="font-bold">Fishing {profileData.fishingLvl} / {profileData.fishingMaxLvl}</p>
-        <p className="font-bold">Mining {profileData.miningLvl} / {profileData.miningMaxLvl}</p>
-        <p className="font-bold">Foraging {profileData.foragingLvl} / {profileData.foragingMaxLvl}</p>
-        <p className="font-bold">Combat {profileData.combatLvl} / {profileData.combatMaxLvl}</p>
+        <p className="font-bold">
+          Farming {profileData.farmingLvl} / {profileData.farmingMaxLvl}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Fishing' : 'Pêche'} {profileData.fishingLvl} / {profileData.fishingMaxLvl}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Mining' : 'Minage'} {profileData.miningLvl} / {profileData.miningMaxLvl}
+        </p>
+        <p className="font-bold">
+          Foraging {profileData.foragingLvl} / {profileData.foragingMaxLvl}
+        </p>
+        <p className="font-bold">
+          Combat {profileData.combatLvl} / {profileData.combatMaxLvl}
+        </p>
       </div>
 
       <div className="rounded-3xl bg-primary text-gray-200 p-5 m-5">
         <h1 className="text-2xl font-bold underline decoration-wavy decoration-yellow decoration-4 underline-offset-8 mb-5">
           Networth :
         </h1>
-        <p className="font-bold">Total Networth : {formatToPrice(profileData.playerTotalNetworth)}</p>
-        <p className="font-bold">Purse : {formatToPrice(profileData.playerPurseNetworth)}</p>
-        <p className="font-bold">Bank : {formatToPrice(profileData.playerBankNetworth)}</p>
-        <p className="font-bold">Sacks : {formatToPrice(profileData.playerSackNetworth)}</p>
-        <p className="font-bold">Armor : {formatToPrice(profileData.playerArmorNetworth)}</p>
-        <p className="font-bold">Equipment : {formatToPrice(profileData.playerEquipmentNetworth)}</p>
-        <p className="font-bold">Wardrobe : {formatToPrice(profileData.playerWardrobeNetworth)}</p>
-        <p className="font-bold">Inventory : {formatToPrice(profileData.playerInventoryNetworth)}</p>
-        <p className="font-bold">Ender Chest : {formatToPrice(profileData.playerEnderChestNetworth)}</p>
-        <p className="font-bold">Accessories : {formatToPrice(profileData.playerAccessoriesNetworth)}</p>
-        <p className="font-bold">Storage : {formatToPrice(profileData.playerStorageNetworth)}</p>
-        <p className="font-bold">Pets : {formatToPrice(profileData.playerPetsNetworth)}</p>
-        <p className="font-bold">Fishing bag : {formatToPrice(profileData.playerFishingBagNetworth)}</p>
-        <p className="font-bold">Museum : {formatToPrice(profileData.playerMuseumNetworth)}</p>
+        <p className="font-bold">
+          {language === 'en' ? 'Total Networth' : 'Networth totale'} : {formatToPrice(profileData.playerTotalNetworth)}
+        </p>
+        <p className="font-bold">
+          Purse : {formatToPrice(profileData.playerPurseNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Bank' : 'Banque'} : {formatToPrice(profileData.playerBankNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Sacks' : 'Sacs'} : {formatToPrice(profileData.playerSackNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Armor' : 'Armure'} : {formatToPrice(profileData.playerArmorNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Equipment' : 'Équipement'} : {formatToPrice(profileData.playerEquipmentNetworth)}
+        </p>
+        <p className="font-bold">
+          Wardrobe : {formatToPrice(profileData.playerWardrobeNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Inventory' : 'Inventaire'} : {formatToPrice(profileData.playerInventoryNetworth)}
+        </p>
+        <p className="font-bold">
+          Ender Chest : {formatToPrice(profileData.playerEnderChestNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Accessories' : 'Accessoires'} : {formatToPrice(profileData.playerAccessoriesNetworth)}
+        </p>
+        <p className="font-bold">
+          Storage : {formatToPrice(profileData.playerStorageNetworth)}
+        </p>
+        <p className="font-bold">
+          Pets : {formatToPrice(profileData.playerPetsNetworth)}
+        </p>
+        <p className="font-bold">
+          Fishing bag : {formatToPrice(profileData.playerFishingBagNetworth)}
+        </p>
+        <p className="font-bold">
+          {language === 'en' ? 'Museum' : 'Musée'} : {formatToPrice(profileData.playerMuseumNetworth)}
+        </p>
       </div>
     </div>
   );
